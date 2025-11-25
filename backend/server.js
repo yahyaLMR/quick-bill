221cb7c590bb126ef28c3ccc9a8d573705ac5858
yahyalmr <yahyalmouri2000@gmail.com>
2025-11-25 22:30:05 +0100

    added connection to the db and commented routes

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,18 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+// Database connection
+const connectDB = require('./config/dbconnect');
+connectDB();
 
+// Routes
+// const settingsRoutes = require('./routes/settingsRoutes');
+// const invoiceRoutes = require('./routes/invoiceRoutes');
+// const userRoutes = require('./routes/userRoutes');
 
-app.use('/api/users', require('./routes/userRoutes'));
-
-
-
+// app.use('/api/settings', settingsRoutes);
+// app.use('/api/invoices', invoiceRoutes);
+// app.use('/api/users', userRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
